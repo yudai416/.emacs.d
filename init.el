cf6ad0e7001bafee391d4f022cf903b43dd505a7
yudai416 <yudai416@gmail.com>
2013-12-01 04:22:46 +0900

    add web-mode

--- init.el
+++ init.el
@@ -222,6 +222,29 @@
   ;; If there is more than one, they won't work right.
  )
 
-;;php-mode
-(load-library "php-mode")
-(require 'php-mode)
+;; ;;php-mode
+;; (load-library "php-mode")
+;; (require 'php-mode)
+
+;;web-mode
+(require 'web-mode)
+;;; 適用する拡張子
+(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+;;; インデント数
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-html-offset   2)
+  (setq web-mode-css-offset    2)
+  (setq web-mode-script-offset 2)
+  (setq web-mode-php-offset    2)
+  (setq web-mode-java-offset   2)
+  (setq web-mode-asp-offset    2))
+(add-hook 'web-mode-hook 'web-mode-hook)
+;;; 色の設定
+(set-face-attribute 'web-mode-html-tag-face nil :foreground "#0000FF")
+(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "#CC9922")
